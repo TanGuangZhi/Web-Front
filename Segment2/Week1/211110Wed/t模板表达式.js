b195f268b8af48b6870cafc91e754200fd81b6c8
“TanGuangZhi” <714763790@qq.com>
2021-11-10 21:20:50 +0800

    js学习

--- Segment2/Week1/211110Wed/t模板表达式.js
+++ Segment2/Week1/211110Wed/t模板表达式.js
@@ -1,5 +1,20 @@
-let num1 = 5;
-let num2 = 10;
+// let num1 = 5;
+// let num2 = 10;
 
-console.log(`${num1} - ${num2}= ${num1 - num2}`);
-console.log(`${num1} - ${num2}= (${num1} - ${num2})`);
+// console.log(`${num1} - ${num2}= ${num1 - num2}`);
+// console.log(`${num1} - ${num2}= (${num1} - ${num2})`);
+
+
+// let width = "500px";
+// let height = "600px";
+
+// console.log(`p元素的宽度是:${width},高度是:${height}`);
+
+
+let flag = "";
+let flag2;
+if (flag2 == undefined) {
+    console.log(true);
+}else{
+    console.log(false);
+}
