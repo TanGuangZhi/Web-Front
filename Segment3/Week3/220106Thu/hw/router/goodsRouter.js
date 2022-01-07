4f0e56ec79b22808098b9f0e3df9d2352c62e761
“TanGuangZhi” <714763790@qq.com>
2022-01-07 12:56:02 +0800

    express study

--- Segment3/Week3/220106Thu/hw/router/goodsRouter.js
+++ Segment3/Week3/220106Thu/hw/router/goodsRouter.js
@@ -1,7 +1,7 @@
 /*
  * @Author: TanGuangZhi
  * @Date: 2022-01-06 19:13:00 Thu
- * @LastEditTime: 2022-01-06 22:00:24 Thu
+ * @LastEditTime: 2022-01-07 09:21:15 Fri
  * @LastEditors: TanGuangZhi
  * @Description: 
  * @KeyWords: Kw
@@ -24,6 +24,19 @@ goodsRouter.post("/insertBook", upload.array("bookImg"), (req, resp) => {
     let data = req.body;
     data.bookImg = commonUtil.upload01(req, "images/book");
     resp.send(goodsDao.insertBook(data) == 1 ? "1" : "0");
+})
+
+goodsRouter.post("/insertBookByBatch", upload.array("bookFile"), (req, resp) => {
+    fs.readFile(req.files[0]?.path, function (error, data) {
+        if (error) {
+            return console.error(error);
+        }
+        // csv to json
+        data = commonUtil.csvParse(data);
+        console.log(data);
+    });
+    // resp.send(goodsDao.insertBook(data) == 1 ? "1" : "0");
+
 })
 
 goodsRouter.post("/delBook", (req, resp) => {
