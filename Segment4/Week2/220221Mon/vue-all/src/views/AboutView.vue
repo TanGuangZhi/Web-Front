e8f3488ffbe9b987d81bb4614974dbae8c85edc2
“TanGuangZhi” <714763790@qq.com>
2022-02-21 13:16:48 +0800

    git in vscode test again

--- Segment4/Week2/220221Mon/vue-all/src/views/AboutView.vue
+++ Segment4/Week2/220221Mon/vue-all/src/views/AboutView.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: TanGuangZhi
+ * @Date: 2022-02-21 10:18:54 Mon
+ * @LastEditTime: 2022-02-21 13:16:20 Mon
+ * @LastEditors: TanGuangZhi
+ * @Description: 
+ * @KeyWords: NodeJs, Express, MongoDB
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page1111</h1>
   </div>
 </template>
