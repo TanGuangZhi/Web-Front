19c21683bad70374f1b9bcaff15c90367f2ce183
氦客船长 <53848822+TanGuangZhi@users.noreply.github.com>
2022-03-06 23:39:24 +0800

    test

--- Segment4/Segment4Proj/orderMeal-WX/orderMeal-WX-End/routes/foodRouter.js
+++ Segment4/Segment4Proj/orderMeal-WX/orderMeal-WX-End/routes/foodRouter.js
@@ -1,7 +1,7 @@
 /*
  * @Author: TanGuangZhi
  * @Date: 2022-02-18 18:51:40 Fri
- * @LastEditTime: 2022-02-26 15:42:26 Sat
+ * @LastEditTime: 2022-03-06 21:43:35 Sun
  * @LastEditors: TanGuangZhi
  * @Description: 
  * @KeyWords: NodeJs, Express, MongoDB
@@ -52,52 +52,4 @@ router.post("/updateFood", async (req, resp) => {
     resp.send(updateObj.modifiedCount > 0 ? "1" : "0");
 });
 
-/**
- * @api {get} /food/loginFood 用户登录
- * @apiDescription 用户登录
- * @apiName loginFood
- * @apiGroup food
- * @apiParam {string} foodName 用户名
- * @apiParam {string} foodPass 密码
- *@apiSuccess {json} result  返回登录成功与失败的状态
- * @apiSuccessExample {text} Success-Response:
- *  "1":成功   "0":失败
- * @apiSampleRequest http://localhost:3000/food/loginFood
- * @apiVersion 1.0.0
- */
-router.get("/loginFood", async (req, resp) => {
-    let foodName = req.query.foodName;
-    let foodPass = req.query.foodPass;
-    let foodArray = await foodModel.loginFood(foodName);
-    if (foodArray.length > 0) {
-        let food = foodArray[0];
-        if (food.status != 1) {
-            resp.send("用户未激活");//用户未激活
-        } else if (food.foodType != 3) {
-            resp.send("用户权限不够");//用户权限不够无法登录
-        } else if (food.foodPass != foodPass) {
-            resp.send("密码不正确");//用户密码不正确
-        } else {
-            //1.th1:token保存的用户信息
-            //2. th2:加密的唯一标识
-            //3.token的存活时间(单位/s)  支持字符串的写法   "1h" 一小时 "1days" 一天
-            // 1296000 = 15day
-            const token = jwt.sign({ food }, "food", { expiresIn: 1296000 });
-            // console.log(token);
-            resp.send(JSON.stringify({ token, food }));
-        }
-    } else {
-        resp.send("用户名不正确");//用户名不正确...
-    }
-});
-
-router.post("/checkToken", (req, resp) => {
-    let token = req.body.token;
-    // //解密 会把token生成时传入的用户信息解密出来
-    let food = jwt.verify(token, 'food');
-    resp.send({
-        code: 200,
-        ...food
-    });
-});
 module.exports = router;
